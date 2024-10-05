37a573730ed226edab6bdd19f9f4303ea43f946a
z_z <chienpham304@gmail.com>
2024-10-05 23:53:44 +0700

    [UI] Setup image caching config

--- ZGithub/Application/AppConfiguration.swift
+++ ZGithub/Application/AppConfiguration.swift
@@ -7,11 +7,28 @@
 //
 
 import Foundation
+import Nuke
 
 final class AppConfiguration {
     private let githubBaseURL = URL(string: "https://api.github.com")!
     
     lazy var githubAPIConfig: GithubAPIConfig = GithubAPIConfig(baseURL: githubBaseURL)
+
+    init() {
+        setupAppConfiguration()
+    }
+
+    private func setupAppConfiguration() {
+        setupIamgeCaching()
+    }
+
+    // MARK: Image caching
+
+    private func setupIamgeCaching() {
+        ImageCache.shared.costLimit = 1024 * 1024 * 200 // 200 MB
+        ImageCache.shared.countLimit = 1000
+        ImageCache.shared.ttl = 120 // Invalidate image after 120 sec
+    }
 }
 
 struct GithubAPIConfig: APIConfig {
