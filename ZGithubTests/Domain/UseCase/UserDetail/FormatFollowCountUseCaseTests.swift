5f683bac0b84a5be34565eef6abf509c7c2a7d95
z_z <chienpham304@gmail.com>
2024-10-07 17:33:19 +0700

    [UnitTest] Revamped FormatFollowCountUseCaseTests test method name

--- ZGithubTests/Domain/UseCase/UserDetail/FormatFollowCountUseCaseTests.swift
+++ ZGithubTests/Domain/UseCase/UserDetail/FormatFollowCountUseCaseTests.swift
@@ -23,31 +23,31 @@ final class FormatFollowCountUseCaseTests: XCTestCase {
         try super.tearDownWithError()
     }
 
-    func testExecute_WithSingleDigit_ReturnsExactValue() {
+    func test_execute_givenSingleDigit_expectExactValue() {
         XCTAssertEqual(useCase.execute(input: 0), "0")
         XCTAssertEqual(useCase.execute(input: 5), "5")
         XCTAssertEqual(useCase.execute(input: 9), "9")
     }
 
-    func testExecute_WithTwoDigits_ReturnsRoundedToNearestTen() {
+    func test_execute_givenTwoDigits_expectRoundedToNearestTen() {
         XCTAssertEqual(useCase.execute(input: 10), "10+")
         XCTAssertEqual(useCase.execute(input: 23), "20+")
         XCTAssertEqual(useCase.execute(input: 99), "90+")
     }
 
-    func testExecute_WithThreeDigits_ReturnsRoundedToNearestHundred() {
+    func test_execute_givenThreeDigits_expectRoundedToNearestHundred() {
         XCTAssertEqual(useCase.execute(input: 100), "100+")
         XCTAssertEqual(useCase.execute(input: 233), "200+")
         XCTAssertEqual(useCase.execute(input: 999), "900+")
     }
 
-    func testExecute_WithThousands_ReturnsRoundedToNearestThousand() {
+    func test_execute_givenThousands_expectRoundedToNearestThousand() {
         XCTAssertEqual(useCase.execute(input: 1000), "1k+")
         XCTAssertEqual(useCase.execute(input: 12345), "12k+")
         XCTAssertEqual(useCase.execute(input: 987654), "987k+")
     }
 
-    func testExecute_WithMillions_ReturnsRoundedToNearestMillion() {
+    func test_execute_givenMillions_expectRoundedToNearestMillion() {
         XCTAssertEqual(useCase.execute(input: 1_000_000), "1m+")
         XCTAssertEqual(useCase.execute(input: 9_234_567), "9m+")
         XCTAssertEqual(useCase.execute(input: 12_345_678), "12m+")
@@ -55,7 +55,7 @@ final class FormatFollowCountUseCaseTests: XCTestCase {
         XCTAssertEqual(useCase.execute(input: 1_122_345_678), "1122m+")
     }
 
-    func testExecute_WithNegativeValue_ReturnsNA() {
+    func test_execute_givenNegativeValue_expectNA() {
         XCTAssertEqual(useCase.execute(input: -1), "N/A")
         XCTAssertEqual(useCase.execute(input: -999), "N/A")
     }
