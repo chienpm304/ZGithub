49b8f8746624b8e522487135b4bcaf11d6790887
z_z <chienpham304@gmail.com>
2024-10-07 15:00:12 +0700

    [Domain] Supported follow formatter with millions

--- ZGithub/Modules/Domain/UseCase/UserDetail/FormatFollowCountUseCase.swift
+++ ZGithub/Modules/Domain/UseCase/UserDetail/FormatFollowCountUseCase.swift
@@ -16,16 +16,19 @@ struct FormatFollowCountUseCase: SyncUseCase {
         roundedCount(input)
     }
 
-    /// Rounds down the given count to the nearest significant unit (tens, hundreds, or thousands) and appends a "+".
+    /// Rounds down the given count to the nearest significant unit (tens, hundreds, thousands, or millions) and appends a "+".
     ///
     /// - Parameter count: The integer value to be formatted.
     /// - Returns: A formatted string representing the rounded down value:
     ///     - `0` to `9`: Returns the exact number as a string (e.g., `7` → `"7"`).
     ///     - `10` to `99`: Rounds down to the nearest 10 and appends `"+"` (e.g., `23` → `"20+"`).
     ///     - `100` to `999`: Rounds down to the nearest 100 and appends `"+"` (e.g., `233` → `"200+"`).
-    ///     - `1000` and `above`: Divides the number by 1000, rounds down to the nearest thousand, and appends `"k+"`
+    ///     - `1000` to `999999`: Divides the number by 1000, rounds down to the nearest thousand, and appends `"k+"`
     ///       (e.g., `1200` → `"1k+"`, `12345` → `"12k+"`).
+    ///     - `1000000` and above: Divides the number by 1,000,000, rounds down to the nearest million, and appends `"m+"`
+    ///       (e.g., `1200000` → `"1m+"`, `12345678` → `"12m+"`).
     private func roundedCount(_ count: Int) -> String {
+        guard count >= 0 else { return "N/A" }
         switch count {
         case 0..<10:
             return "\(count)"
@@ -33,8 +36,10 @@ struct FormatFollowCountUseCase: SyncUseCase {
             return "\(count - count % 10)+"
         case 100..<1000:
             return "\(count - count % 100)+"
-        default:
+        case 1000..<1_000_000:
             return "\(count / 1000)k+"
+       default:
+            return "\(count / 1_000_000)m+"
         }
     }
 }
